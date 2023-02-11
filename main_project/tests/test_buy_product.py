3f025f4ada4ec0882578909cc4d81fdfecaa2745
Gleb AQA <galievgleb99@gmail.com>
2023-02-11 08:19:27 +0300

    add options.add_experimental_options for cosole hiding warnings

--- main_project/tests/test_buy_product.py
+++ main_project/tests/test_buy_product.py
@@ -1,16 +1,18 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 from pages.login_page import Login_page
 
 
 def test_select_product():  # создаем метод
+    options = Options()
+    options.add_experimental_option("excludeSwitches", ['enable-logging'])
     driver = webdriver.Chrome(
-        executable_path='C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\chromedriver.exe')  # Указали директорию в которой находится хром драйвер, для того чтобы мы могли обращаться к браузеру гугл хром
+        executable_path='C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\chromedriver.exe', chrome_options=options)  # Указали директорию в которой находится хром драйвер, для того чтобы мы могли обращаться к браузеру гугл хром
 
     print("Start Test")
 
