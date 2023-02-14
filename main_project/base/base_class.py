cdde46eb0c32b010cdb0665e95a5e0bf8ed8c67a
Gleb AQA <galievgleb99@gmail.com>
2023-02-14 21:19:27 +0300

    add function in base_class

--- main_project/base/base_class.py
+++ main_project/base/base_class.py
@@ -8,3 +8,8 @@ class Base():
     def get_current_url(self):
         get_url = self.driver.current_url
         print("Current url " + get_url)
+
+         """Method assert word"""
+
+    def assert_word(self, word)
+
