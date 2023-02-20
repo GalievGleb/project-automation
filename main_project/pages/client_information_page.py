3bc8e93de2bde1926bc6a26bbfa89d049d5d8d66
Gleb AQA <galievgleb99@gmail.com>
2023-02-20 20:34:10 +0300

    add client info page

---
+++ main_project/pages/client_information_page.py
@@ -0,0 +1,60 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+
+
+class Client_information_page(Base):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators
+    first_name = "[id='first-name']"
+    last_name = "[id='last-name']"
+    postal_code = "[id='postal-code']"
+    continue_button = "[id='continue']"
+
+    # Getters
+
+    def get_first_name(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.first_name)))
+
+    def get_last_name(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.last_name)))
+
+    def get_postal_code(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.postal_code)))
+
+    def get_continue_button(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.continue_button)))
+
+    # Action
+    def input_first_name(self, first_name):
+        self.get_first_name().send_keys(first_name)
+        print("Input first name")
+
+    def input_last_name(self, last_name):
+        self.get_last_name().send_keys(last_name)
+        print("Input password")
+
+    def input_postal_code(self, postal_code):
+        self.get_postal_code().send_keys(postal_code)
+        print("Input password")
+    def click_continue_button(self):
+        self.get_continue_button().click()
+        print("Click login button")
+
+    # Methods
+    def input_information(self):
+        self.get_current_url()
+        self.input_first_name("Gleb")
+        self.input_last_name("Galiev")
+        self.input_postal_code("1234")
+        self.click_continue_button()
--- main_project/tests/test_buy_product.py
+++ main_project/tests/test_buy_product.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+from pages.cart_page import Cart_page
+from pages.client_information_page import Client_information_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
 
@@ -20,7 +22,14 @@ def test_select_product():  # создаем метод
 
     login = Login_page(driver)
     login.authorization()
+
     mp = Main_page(driver)
     mp.select_product()
+
+    cp = Cart_page(driver)
+    cp.click_checkout_button()
+
+    cip = Client_information_page(driver)
+    cip.input_information()
 
     time.sleep(5)
